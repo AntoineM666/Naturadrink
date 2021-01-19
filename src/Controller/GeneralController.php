b695c031192ad6de1c7a3bd0edbf5291ee7680b6
Antoine MARIE <antoine.marie62126@gmail.com>
2021-01-19 16:00:59 +0100

    ajout du module de paiement

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+
+use Stripe\Stripe;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -80,6 +84,55 @@ class GeneralController extends AbstractController
         ]);
 
         
+    }
+
+    /**
+    * @Route("{id}/create-checkout-session", name="checkout")
+    */
+    public function checkout(Product $product)
+    {
+        \Stripe\Stripe::setApiKey('sk_test_51HtZJ1H9uCMtPurA36KmUuJJJPfasqruJr6Hz6L7JURruYCGBsnZpJ7ONrf67ZoJRsSR0ZSjAtgs7vLb8QJLLKXU00zHUcvVAJ');
+
+        
+        
+            $session = \Stripe\Checkout\Session::create([
+            
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => $product->getTitle(),
+                  
+                ],
+                'unit_amount' => $product->getPrice()*100,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('success',[],UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('error',[],UrlGeneratorInterface::ABSOLUTE_URL),
+
+          ]);
+
+              return new JsonResponse( [ 'id' => $session->id ] );
+
+    }
+
+    /**
+     * @Route("/success" , name="success")
+     */
+    public function success()
+    {
+        return $this->render('general/success.html.twig');
+    }
+
+    /**
+     * @Route("/error", name="error")
+     */
+    public function error()
+    {
+        return $this->render('general/error.html.twig');
     }
 
 
